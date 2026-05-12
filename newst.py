9ca4c7ea8ef771920c4a8d65a5843590b2bec9f1
shilling <shilling@gachon.ac.kr>
2026-05-12 18:07:30 +0900

    gaon

--- newst.py
+++ newst.py
@@ -1,11 +1,5 @@
 print('gachom')
 print('시발')
-<<<<<<< HEAD
-for i in range(1,10):
-    print('안녕하세요')
-    
-    
-=======
 print('가온')
 print('서빈')
->>>>>>> 470f93b3c02cea4190e4cea74209435bf567e56d
+
